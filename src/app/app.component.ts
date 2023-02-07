b92e932655a563524c3583119a5a84e0d5d3fa2f
Karol Depka <karol-depka@users.noreply.github.com>
2023-02-07 02:32:09 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'test_angular_15_for_material_themes_error';
+}
